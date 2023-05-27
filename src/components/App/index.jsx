2644784a0859b864b2b5c38b7a9c9a8af09dce95
Iva Havranová <havranova.i@seznam.cz>
2023-05-27 10:06:21 +0200

    routing in progress

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router={router} />
+    {/* <Home /> */}
     <Footer />
   </>
 );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/reservation',
+    element: <h2>Detail jízdenky</h2>,
+  },
+]);
