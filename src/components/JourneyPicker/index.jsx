626fbeae7cef92b52c7055a1dc12c5783e785049
Iva Havranová <havranova.i@seznam.cz>
2023-05-27 10:42:27 +0200

    CityOptions component created

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
-// Napojte každý ze stavů na správý <select> tak, aby select zobrazoval vybraný stav a změna v selectu se promítla do stavu. Vytvoříte tedy dvoucestný databinding, kdy se např. stav fromCity bude promítat do value příslušného selectu, a při události onChange na selectu se nová hodnota zapíše do stavu fromCity. Obdobně i pro další dva selecty a stavy toCity a date.
+// Přímo v souboru index.jsx komponenty JourneyPicker si vytvořte novou komponentu CityOptions. Přesuňte do ní všechny <option> ze selectu pro výběr výchozího města. Komponentu CityOptions použijte v obou selectech pro výběr města. Zkontrolujte, že v prohlížeči vše funguje stejně.
 export const JourneyPicker = ({ onJourneyChange }) => {
   const [fromCity, setFromCity] = useState('');
   const [toCity, setToCity] = useState('');
@@ -8,6 +8,18 @@ export const JourneyPicker = ({ onJourneyChange }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(fromCity, toCity, date);
+  };
+  const CityOptions = () => {
+    return (
+      <>
+        <option value="">Vyberte</option>
+        <option value="mesto01">Město 01</option>
+        <option value="mesto02">Město 02</option>
+        <option value="mesto03">Město 03</option>
+        <option value="mesto04">Město 04</option>
+        <option value="mesto05">Město 05</option>
+      </>
+    );
   };
   return (
     <div className="journey-picker container">
@@ -21,12 +33,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
                 setFromCity(e.target.value);
               }}
             >
-              <option value="">Vyberte</option>
-              <option value="mesto01">Město 01</option>
-              <option value="mesto02">Město 02</option>
-              <option value="mesto03">Město 03</option>
-              <option value="mesto04">Město 04</option>
-              <option value="mesto05">Město 05</option>
+              <CityOptions />
             </select>
           </label>
           <label>
@@ -36,12 +43,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
                 setToCity(e.target.value);
               }}
             >
-              <option value="">Vyberte</option>
-              <option value="mesto01">Město 01</option>
-              <option value="mesto02">Město 02</option>
-              <option value="mesto03">Město 03</option>
-              <option value="mesto04">Město 04</option>
-              <option value="mesto05">Město 05</option>
+              <CityOptions />
             </select>
           </label>
           <label>
