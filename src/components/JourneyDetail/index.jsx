d89a1511baf6b6b889f3a30817b16b5ae1bab97b
Iva Havranová <havranova.i@seznam.cz>
2023-05-27 15:00:36 +0200

    journey details done

--- src/components/JourneyDetail/index.jsx
+++ src/components/JourneyDetail/index.jsx
@@ -3,13 +3,24 @@ import './style.css';
 import { BusStop } from '../BusStop';
 
 export const JourneyDetail = ({ journey }) => {
-  console.log(journey.stops);
+  const stops = journey.stops;
   return (
     <>
       <div className="journey-detail container">
         <h2>Podrobnosti cesty</h2>
         <div className="stops">
           <div className="container">
+            {stops.map((stop) => {
+              return (
+                <BusStop
+                  name={stop.name}
+                  station={stop.station}
+                  time={stop.time}
+                  key={stop.code}
+                />
+              );
+            })}
+
             <BusStop name="Praha" station="ÚAN Florenc" time="15:55" />
           </div>
         </div>
