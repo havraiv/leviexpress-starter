d63da4c84611805d387f5e2d652d4e9dda94e920
Iva Havranová <havranova.i@seznam.cz>
2023-05-27 14:51:59 +0200

    BusStop in progress

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 import { JourneyDetail } from '../JourneyDetail';
-// Použijete komponentu JourneyDetail v komponentě Home na místě, kde se nyní vypisuje id nalezeného spoje. Komponenta se bude zobrazovat jenom tehdy, když ve stavu journey v komponentě Home je něco jiného, než null. Ověřte, že po vyhledání spojení se na stránce zobrazí podrobnosti cesty s městy 1 až 4.
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
@@ -12,7 +11,7 @@ export const Home = () => {
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
-      {journey !== null ? <JourneyDetail /> : false}
+      {journey !== null ? <JourneyDetail journey={journey} /> : false}
     </main>
   );
 };
--- src/components/JourneyDetail/index.jsx
+++ src/components/JourneyDetail/index.jsx
@@ -1,43 +1,16 @@
 import React from 'react';
 import './style.css';
+import { BusStop } from '../BusStop';
 
-export const JourneyDetail = () => {
+export const JourneyDetail = ({ journey }) => {
+  console.log(journey.stops);
   return (
     <>
       <div className="journey-detail container">
         <h2>Podrobnosti cesty</h2>
         <div className="stops">
-          <div className="bus-stop">
-            <div className="bus-stop__bullet"></div>
-            <div className="bus-stop__place">
-              <div className="bus-stop__city">Město 1</div>
-              <div className="bus-stop__station">Zastávka</div>
-            </div>
-            <div className="bus-stop__departure">10:45</div>
-          </div>
-          <div className="bus-stop">
-            <div className="bus-stop__bullet"></div>
-            <div className="bus-stop__place">
-              <div className="bus-stop__city">Město 2</div>
-              <div className="bus-stop__station">Zastávka</div>
-            </div>
-            <div className="bus-stop__departure">10:45</div>
-          </div>
-          <div className="bus-stop">
-            <div className="bus-stop__bullet"></div>
-            <div className="bus-stop__place">
-              <div className="bus-stop__city">Město 3</div>
-              <div className="bus-stop__station">Zastávka</div>
-            </div>
-            <div className="bus-stop__departure">10:45</div>
-          </div>
-          <div className="bus-stop">
-            <div className="bus-stop__bullet"></div>
-            <div className="bus-stop__place">
-              <div className="bus-stop__city">Město 4</div>
-              <div className="bus-stop__station">Zastávka</div>
-            </div>
-            <div className="bus-stop__departure">10:45</div>
+          <div className="container">
+            <BusStop name="Praha" station="ÚAN Florenc" time="15:55" />
           </div>
         </div>
       </div>
